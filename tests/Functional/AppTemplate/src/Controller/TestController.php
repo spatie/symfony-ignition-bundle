568c3bf353f854d1a48ce315cb66091452fd4d09
Andrew MacRobert <andrew.macrobert@gmail.com>
2022-01-25 10:54:23 -0500

    Test the bundle on supported PHP and Symfony versions

---
+++ tests/Functional/AppTemplate/src/Controller/TestController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\{JsonResponse, Response};
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    public function index(): Response
+    {
+        $symfonyVersion = \Symfony\Component\HttpKernel\Kernel::VERSION;
+        $phpVersion = PHP_VERSION;
+
+        throw new \Exception(sprintf('Test Exception: Symfony %s PHP %s', $symfonyVersion, $phpVersion));
+        return new Response('We should never see this');
+    }
+}
